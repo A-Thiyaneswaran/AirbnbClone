bbf17e49c3ab55cd4da21290666e94990ae14462
A-Thiyaneswaran <thyanesh23@gmail.com>
2024-02-01 16:50:53 +0530

    changing state of like button

--- src/Components/RowCards/RowCards.jsx
+++ src/Components/RowCards/RowCards.jsx
@@ -16,20 +16,31 @@ function RowCards() {
   ]
 
   const [loading, setLoading] = useState(true)
-  const [likebtnColor, setlikebtnColor] = useState("#555");
- 
-    const changeStyle = () => {
-        console.log("you just clicked");
-        if (likebtnColor !== "#555") setlikebtnColor("#555");
-        else setlikebtnColor("#ffc0cb");
-    }
+  //const [likebtnColor, setlikebtnColor] = useState("#555");
+  const [likedCards, setLikedCards] = useState(Array(details.length).fill("#555"));
+
+  const handleLike = (index) => {
+    // Create a copy of likedCards
+    const updatedLikedCards = [...likedCards];
+    // Toggle the liked state of the clicked card
+    if (updatedLikedCards[index] != "#555")
+      updatedLikedCards[index]="#555";
+    else
+    updatedLikedCards[index] = "#ffc0cb";
+    // Update the state
+    setLikedCards(updatedLikedCards);
+  };
+
+    // const changeStyle = () => {
+    //     if (likebtnColor !== "#555") setlikebtnColor("#555");
+    //     else setlikebtnColor("#ffc0cb");
+    // }
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false)
     }, 2000);
   }, [])
-  
 
   return (
     <div className='w-full pt-4 flex flex-col lg:grid lg:grid-cols-4 gap-8 md:gap-6 lg:gap-6 px-6 md:px-[2.5rem] lg:px-[5.5rem] 2xl:px-[5rem]'>
@@ -45,7 +56,7 @@ function RowCards() {
                     <span className="like text-xl">
                       <svg width="100%" height="100%" viewBox="0 -3.71 75.17 75.17" xmlns="http://www.w3.org/2000/svg">
                         <path id="Path_1" data-name="Path 1" d="M117.606,280.375s22.263-15.459,31.959-30.318c9.6-14.708.354-31.054-10.533-33.8-14.457-3.65-21.426,10.478-21.426,10.478s-6.968-14.128-21.425-10.478c-10.888,2.748-20.132,19.094-10.534,33.8C95.343,264.916,117.606,280.375,117.606,280.375Z" transform="translate(-80.021 -214.131)"
-                         fill = {likebtnColor} stroke="#fff" strokeLinejoin="round" strokeWidth="5" onClick={changeStyle}/>
+                         fill = {likedCards[index]} stroke="#fff" strokeLinejoin="round" strokeWidth="5" onClick={() => handleLike(index)}/>
                       </svg>
                     </span>
                   </div>
